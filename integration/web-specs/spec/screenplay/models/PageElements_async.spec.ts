e312935244fb1fb639ab82fcfe12e20723b333d8
viper3400 <viper_3400@gmx.de>
2022-01-24 18:25:19 +0100

    test(web): use case for async loaded page elements

---
+++ integration/web-specs/spec/screenplay/models/PageElements_async.spec.ts
@@ -0,0 +1,108 @@
+import 'mocha';
+
+import { contain, Ensure, equals, startsWith } from '@serenity-js/assertions';
+import { actorCalled, Answerable, Check, Duration, Log } from '@serenity-js/core';
+import { By, Click, CssClasses, Navigate, Page, PageElement, PageElements, Text, Wait } from '@serenity-js/web';
+
+
+class ShoppingList {
+    static app = () =>
+        PageElement.located(By.id('shopping-list-app'))
+            .describedAs('shopping list app');
+
+    static progress = () =>
+        PageElement.located(By.css('.progress'))
+            .describedAs('progress bar')
+            .of(ShoppingList.app());
+
+    static numberOfItemsLeft = () =>
+        PageElement.located(By.css('span'))
+            .describedAs('number of items left')
+            .of(ShoppingList.progress());
+
+    static header = () =>
+        PageElement.located(By.tagName('h1')).describedAs('header');
+
+    static list = () =>
+        PageElement.located(By.tagName('ul')).describedAs('shopping list');
+
+    static items = () =>
+        PageElements.located(By.tagName('li'))
+            .describedAs('items')
+            .of(ShoppingList.app());
+
+    static boughtItems = () =>
+        PageElements.located(By.css('.bought'))
+            .describedAs('bought items')
+            .of(ShoppingList.list());
+}
+
+class DynamicShoppingList {
+    static items = () =>
+        PageElements.located(By.css('li')).describedAs('shopping list items');
+
+    static item = () =>
+        PageElement.located(By.css('li')).describedAs('shopping list item');
+
+    static titles = () =>
+        PageElements.located(By.css('li span.item-name')).describedAs('shopping list item titles');
+
+    static itemName = () =>
+        PageElement.located(By.css('span.item-name')).describedAs('item name');
+
+    static output = () =>
+        PageElement.located(By.css('#output')).describedAs('output');
+}
+
+const ItemCalled = (name: string) =>
+    DynamicShoppingList.items()
+        .where(Text.of(DynamicShoppingList.itemName()), equals(name))
+        .first();
+
+const ItemsLeftToBuy = () =>
+    DynamicShoppingList.items()
+        .where(CssClasses, contain('buy'));
+
+const LinkTo = (item: Answerable<PageElement>) =>
+    PageElement.located(By.css('a')).of(item).describedAs('link to element');
+
+/** @test {PageElements} */
+describe('PageElements', () => {
+
+    describe('allows the actor to locate', () => {
+
+        it('a single web element matching the selector', () =>
+            actorCalled('Elle').attemptsTo(
+                Navigate.to('/screenplay/models/page-elements/shopping_list.html'),
+
+                Ensure.that(Text.of(ShoppingList.header()), equals('Shopping list')),
+            ));
+
+        it('all web elements matching a selector', () =>
+            actorCalled('Elle').attemptsTo(
+                Navigate.to('/screenplay/models/page-elements/shopping_list.html'),
+
+                Ensure.that(Text.ofAll(ShoppingList.items()), contain('oats')),
+            ));
+
+        it('an element relative to another page element', () =>
+            actorCalled('Elle').attemptsTo(
+                Navigate.to('/screenplay/models/page-elements/shopping_list.html'),
+                Ensure.that(ShoppingList.numberOfItemsLeft().text().as(Number), equals(2)),
+            ));
+
+        it('can handle timeout' , () =>
+            actorCalled('Elle').attemptsTo(
+                Navigate.to('/screenplay/models/page-elements/dynamic_shopping_list_async.html'),
+                Click.on(LinkTo(ItemCalled('coffee'))),
+                //Wait.for(Duration.ofSeconds(10)),
+                //Ensure.that(Text.of(DynamicShoppingList.output()), equals( 'clicked on: coffee x' ))
+                Check.whether(Text.of(DynamicShoppingList.output()), equals( 'clicked on: coffee x' ))
+                .andIfSo(
+                    Ensure.that(Text.of(DynamicShoppingList.output()), equals( 'clicked on: coffee x' )
+                )).otherwise(
+                    Log.the('Text not found.')
+                )
+        ));
+    });
+});
